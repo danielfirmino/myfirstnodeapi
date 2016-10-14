45e42a30386ff143d0c6f01f01116ac0c9a2c188
f1rm1n0 <daniel.firmino@gmail.com>
2016-10-14 16:25:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP;
  var http = require('http');
 
