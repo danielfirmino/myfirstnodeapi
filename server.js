1e9d6ca584c21caa1eed61aded73eda3b8cacdbf
f1rm1n0 <daniel.firmino@gmail.com>
2016-10-14 15:28:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-var server_port = process.env.OPENSHIFT_NODEJS_PORT;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP;
-var http = require('http');
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8087
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ var http = require('http');
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Node.JS!');
-}).listen(server_port);
-console.log('Server running at http://localhost:' + server_port);
+  console.log( "Listening on " + server_ip_address + ", port " + server_port );
+}).listen(server_port, server_ip_address);
+
+console.log( "Listening on " + server_ip_address + ", port " + server_port );
