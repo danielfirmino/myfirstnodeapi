41de39d9c380c7abfce06cde8b63d1bfd21cc5e8
f1rm1n0 <daniel.firmino@gmail.com>
2016-10-14 13:35:51 -0300

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,6 +1,8 @@
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8086
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Node.JS!');
-}).listen(8080);
-console.log('Server running at http://localhost:8080/');
+}).listen(server_port);
+console.log('Server running at http://localhost:' + server_port);
